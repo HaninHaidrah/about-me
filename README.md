12ba97f4d6778b86b0193845b566e56d156efa07
HaninHaidrah <haneenhadey@gmail.com>
2021-07-06 21:38:33 +0300

    practice loops and css

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # about-me
 
-# LAB - 02
+# LAB - 03
 * About Me
 
-I build *about-me* website with a direct and powerful purpose. It does all of the things that it accomplishes well. It is a web for gussing game about me to get to know better.
+I build *about-me* website with a direct and powerful purpose. It does all of the things that it accomplishes well. It is a web for gussing game about me to get to know better by asking the user firstly five yes or no funny questions just to warm him up then other two questions about me the first on needs answer with a number with 4 attempts and give the user feedback whike he is answering wrong and the other question is for make him curious to know me better with 6 limited try if he can guess one of them I will show him the others.
 
 
 * Author: HaninHaidrah
@@ -11,5 +11,7 @@ I build *about-me* website with a direct and powerful purpose. It does all of th
 * submission PR: still empty
 * Any Links you used as reference: no linkes yet
 * Reflections and Comments
-Consider including the answers to your daily journal and submission questions here
-This is also a good place to reflect on the tools and resources used and learned
+1. I added two questions to my game one needs a numberic output and it was challanging 
+2. The other question was adding an array to store multiple choices and it was really the hardest and mose intresting one
+3. doing styling "simple one " to html
+4. using loops in my codes
--- js/app.js
+++ js/app.js
@@ -1,25 +1,35 @@
 'use strict';
 
 let yourname = prompt('what\s your name?');
+
 alert('glad to join me ' + yourname + ' in this game to have fun');
+
 alert('so,let\'s start');
-alert(' Its easy. answer the 5 gussing questions about me ');
+
+alert(' Its easy. answer the 7 gussing questions about me ');
+
+let score = 0;
 
 
+
+// Question 1:
 
 let myorgin = prompt('Am I from Jordan?');
+
 myorgin = myorgin.toLowerCase();
 
-if (myorgin === 'yes' || myorgin==='y') {
+if (myorgin === 'yes' || myorgin === 'y') {
 
     // console.log(myorgin);
-    alert('Opps, your wrong ' + yourname + ' I\'m from Yemen')
+    alert(`Nope, your wrong ${yourname} I\'m from Yemen`)
 }
 
-else if (myorgin === 'no' || myorgin==='n') {
+else if (myorgin === 'no' || myorgin === 'n') {
 
-    alert('Thats coreect' + yourname + ' I\'m from Yemen')
+    alert(`Thats coreect ${yourname} I\'m from Yemen`)
     // console.log(myorgin);
+
+    score++;
 }
 
 else {
@@ -29,101 +39,194 @@ else {
 
 
 
+//  Question2:
 
-
-
-let myage = prompt('Im 25 now , If I was born 3 years age, would my age be 22');
+let myage = prompt('Im 25 now , If I was born 3 years age, would my age be 22')
 myage = myage.toLowerCase();
 
-if (myage === 'yes' || myage==='y') {
+if (myage === 'yes' || myage === 'y') {
 
     // console.log(myage);
-    alert('Opps, your wrong ' + yourname + ' I would be 3 years only, Think about it')
+    alert(`Are you fine! ${yourname} I would be 3 years only, Think about it`);
 }
 
-else if (myage === 'no' || myagee=== 'n') {
+else if (myage === 'no' || myage === 'n') {
 
-    alert('Oh your right ' + yourname + ' your clever')
+    alert(`youre a wake, good job ${yourname}`);
     // console.log(myage);
+    score++;
+
 }
 
 else {
 
-    alert('your answer should be yes or no . try again ')
+    alert('your answer should be yes or no . try again ');
 }
 
 
 
 
+// Question3:
 
 let mycountry = prompt('Does Jordan have many seas without any water like Yemen ?');
+
 mycountry = mycountry.toLowerCase();
 
-if (mycountry === 'yes' || mycountry==='y') {
+if (mycountry === 'yes' || mycountry === 'y') {
 
-    console.log(mycountry);
-    alert('Thats cool, your right ' + ' its on the map, its funny')
+    // console.log(mycountry);
+
+    alert('Thats cool, your right ' + ' its on the map, its funny');
+
+    score++;
+
 }
 
-else if (mycountry === 'no' || mycountry==='n') {
+else if (mycountry === 'no' || mycountry === 'n') {
 
-    alert('Opps, your wrong ' + yourname + ' its on the map, its funny')
-    console.log(mycountry);
+    alert('Opps, your wrong ' + yourname + ' its on the map, its funny');
+    // console.log(mycountry);
 }
 
 else {
 
-    alert('your answer should be yes or no . try again ')
+    alert('your answer should be yes or no . try again ');
 }
 
 
 
 
 
-
+// Question4:
 let mydrinks = prompt('Do I drink hot drinks in summer?');
+
 mydrinks = mydrinks.toLowerCase();
 
-if (mydrinks === 'yes' ||mydrinks=== 'y') {
+if (mydrinks === 'yes' || mydrinks === 'y') {
 
-    console.log(mydrinks);
-    alert('Thats cool, your right' + ' because I cant eat them, its funny')
+    alert(`drinks cant be eaten, good job`);
+
+    score++;
+
 }
 
-else if (mydrinks === 'no' || mydrinks=== 'n') {
+else if (mydrinks === 'no' || mydrinks === 'n') {
 
-    alert('Opps, your wrong' + yourname + ' I dink them because I cant eat them, its funny')
-    console.log(mydrinks);
+    alert(`Absoultly yes, think about it ${yourname}, we cant drink them`);
+    // console.log(mydrinks);
 }
 
 else {
 
-    alert('your answer should be yes or no . try again ')
+    alert('your answer should be yes or no . try again ');
 }
 
 
 
+
+// Question5:
 
 let pets = prompt('Are pets annoying me?');
 pets = pets.toLowerCase();
 
-if (pets === 'yes' || pets==='y') {
+if (pets === 'yes' || pets === 'y') {
 
     // console.log(pets);
-    alert('Thats cool, your right' + ' I hate them')
+    alert('Thats cool, your right' + ' I hate them');
+
+    score++;
+
 }
 
-else if (pets === 'no' || pets=== 'n') {
+else if (pets === 'no' || pets === 'n') {
 
-    alert('Opps, your wrong' + yourname)
+    alert('Opps, your wrong' + yourname);
     // console.log(pets);
 }
 
 else {
 
-    alert('your answer should be yes or no . try again ')
+    alert('your answer should be yes or no . try again ');
 }
 
 
 
 alert('It was a funny game ' + yourname + 'now  you will get to know better');
+
+
+
+// Question6:
+let number = Number(prompt('Since when I have been staying in Jordan? choose a number from 1996 to 2021'))
+
+
+for (let i = 1; i < 4; i++) {
+
+
+
+    if (number === 2015) {
+
+        alert(' Its awsome you got it');
+        score++;
+
+        break;
+    }
+
+    else if (number > 2015) {
+        alert('its too high')
+        number = Number(prompt('opps, try again'));
+
+
+    }
+
+    else if (number < 2015) {
+
+        alert('its too low')
+        number = Number(prompt('opps, try again'));
+
+
+    }
+
+}
+
+
+alert('The answer is 2015');
+
+
+
+
+// Question7:
+
+let visitedcountry = prompt('Which countries that Ive visted them?')
+let country = ['syria', 'turkey', 'egypt', 'libya']
+let answer = 'false'
+for (let i = 0; i <= 4; i++) {
+    for (let j = 0; j < country.length; j++) {
+        // console.log(country)
+
+
+
+
+        if (visitedcountry == country[j]) {
+
+            answer = 'true'
+            alert('good job');
+            score++;
+            break;
+
+        }
+
+    }
+
+    if (answer == 'true') {
+
+        break;
+    }
+    else {
+        alert('opps,I hope ill visit it');
+        visitedcountry = prompt('try again');
+    }
+}
+
+alert(country)
+
+alert(`you got ${score} out of 7`)
